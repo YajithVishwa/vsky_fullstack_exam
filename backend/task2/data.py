11ca4dbd72339b481d7fed0f299d42e9102ca1b7
YajithVishwa <yajithvishwa2001@gmail.com>
2024-08-29 21:15:37 +0530

    task2 completed

--- backend/task2/data.py
+++ backend/task2/data.py
@@ -1,5 +1,38 @@
+list_of_orders = [
+    {
+        "item_name": "Mouse",
+        "quantity": 1,
+        "price_per_item": 3.50
+    },
+    {
+        "item_name": "Keyboard",
+        "quantity": 6,
+        "price_per_item": 12
+    },
+    {
+        "item_name": "Monitor",
+        "quantity": 2,
+        "price_per_item": 10
+    }
+]
 
+def get_data(list_of_orders) -> float:
+    total_revenue = 0
+    def order_input_validation(order:dict) -> bool:
+        if "item_name" in order and "quantity" in order and "price_per_item" in order:
+            if isinstance(order['quantity'], int) and (isinstance(order['price_per_item'], int) or isinstance(order['price_per_item'], float)):
+                return True
+        return False
+    if not isinstance(list_of_orders, list):
+        print('List of Order input is invalid')
+        return 0
+    if len(list_of_orders) == 0:
+        return 0
+    for order in list_of_orders:
+        if (not isinstance(order, dict)) or not order_input_validation(order):
+            print('List of Order input is invalid')
+            return 0
+        total_revenue += order['quantity'] * order['price_per_item']
+    return total_revenue
 
-def get_data() -> float:
-    # Write your code here
-    return
+print('Total Revenue - ', get_data(list_of_orders))
